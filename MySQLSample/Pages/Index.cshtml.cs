faa4af20dab67cab752588065d253a369d35fc95
okayu <yuto.gio.0515@gmail.com>
2024-09-13 22:11:37 +0900

    DBからデータ取得し、画面表示

--- MySQLSample/Pages/Index.cshtml.cs
+++ MySQLSample/Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MySQLSample.Data;
+using MySQLSample.Models;
 
 namespace MySQLSample.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly MySQLSampleContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IList<User> Users { get; set; } = default!;
+
+        public IndexModel(ILogger<IndexModel> logger, MySQLSampleContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            Users = await _context.User.ToListAsync();
         }
     }
 }
