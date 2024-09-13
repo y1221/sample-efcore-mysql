355d309dd4d610ef449230aea1d5b587f7a93d10
okayu <yuto.gio.0515@gmail.com>
2024-09-13 09:41:43 +0900

    モデル追加

---
+++ MySQLSample/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MySQLSample.Models
+{
+    public class User
+    {
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+
+        [Display(Name = "ユーザ名")]
+        [DataType(DataType.Text)]
+        [StringLength(20, ErrorMessage = "20文字以下で入力してください。")]
+        [Required(ErrorMessage = "必須項目です。")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
