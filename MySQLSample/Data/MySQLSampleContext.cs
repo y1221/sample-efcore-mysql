6ca2f4a96547f3f71afa4eeccaebc881226db96f
okayu <yuto.gio.0515@gmail.com>
2024-09-13 19:52:41 +0900

    DbContext作成

---
+++ MySQLSample/Data/MySQLSampleContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MySQLSample.Data
+{
+    public class MySQLSampleContext : DbContext
+    {
+        public MySQLSampleContext (DbContextOptions<MySQLSampleContext> options) : base(options) { }
+
+        public DbSet<MySQLSample.Models.User> User { get; set; } = default!;
+    }
+}
