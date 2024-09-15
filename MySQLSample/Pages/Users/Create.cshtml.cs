6d689b664fb5446f1f3a437428b71e2a9bacb52e
okayu <yuto.gio.0515@gmail.com>
2024-09-15 17:20:43 +0900

    スキャフォールディング

---
+++ MySQLSample/Pages/Users/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MySQLSample.Data;
+using MySQLSample.Models;
+
+namespace MySQLSample.Pages.Users
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MySQLSample.Data.MySQLSampleContext _context;
+
+        public CreateModel(MySQLSample.Data.MySQLSampleContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.User.Add(User);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ MySQLSample/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MySQLSample.Data;
+using MySQLSample.Models;
+
+namespace MySQLSample.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MySQLSample.Data.MySQLSampleContext _context;
+
+        public IndexModel(MySQLSample.Data.MySQLSampleContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            User = await _context.User.ToListAsync();
+        }
+    }
+}
